87f90deca97136e7ac0b4e8b9a0b97a3799e25e7
Jordi Miguel <jmiguel@gnuine.com>
2013-04-02 16:33:56 +0200

    add definition to change named options

---
+++ manifests/definitions/bind-options.pp
@@ -0,0 +1,22 @@
+/*
+
+= Definition: bind::options
+Sets named.conf options with template file
+
+Arguments:
+ *$template_file*: Source template file for named.conf
+
+*/
+define bind::options($template_file) {
+
+  file { "${bind::params::config_home}/named.conf":
+    ensure => file,
+    content => template($template_file),
+    owner => 'root',
+    group => 'named',
+    mode => 0640,
+    notify => Service["bind9"],
+    require => Package["bind9"],
+  }
+
+}
